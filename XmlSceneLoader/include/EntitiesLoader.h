b5fbe43111e9a0a24fa2f36352ce5cb31cf2e786
Eaglegor <eaglegor@gmail.com>
2014-08-09 22:13:33 +0400

    Scene loader supports data providers

--- XmlSceneLoader/include/EntitiesLoader.h
+++ XmlSceneLoader/include/EntitiesLoader.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <boost/property_tree/ptree_fwd.hpp>
+#include <unordered_map>
 
 namespace UnknownEngine
 {
@@ -23,6 +24,8 @@ namespace UnknownEngine
 
 	namespace Loader
 	{
+
+		class IDataProvider;
 		class XmlSceneLoader;
 
 		class EntitiesLoader
@@ -43,6 +46,8 @@ namespace UnknownEngine
 				Core::Component* loadComponent ( Core::Entity* parent_entity, const std::string &name, const boost::property_tree::ptree &component_node );
 				bool createDataProvider ( const boost::property_tree::ptree &data_provider_node );
 
+				std::unordered_map<std::string, Loader::IDataProvider*> data_providers;
+				
 				Core::EngineContext* engine_context;
 				XmlSceneLoader* scene_loader;
 		};
