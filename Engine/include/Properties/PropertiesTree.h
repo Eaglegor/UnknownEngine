d0f0bff323e48a58df0f59ace79a347eaeb611a6
Eaglegor <eaglegor@gmail.com>
2014-08-16 15:30:27 +0400

    Iterator to iterate over properties of specified type

--- Engine/include/Properties/PropertiesTree.h
+++ Engine/include/Properties/PropertiesTree.h
@@ -25,7 +25,7 @@ namespace UnknownEngine
 		* @brief Is thrown when requested a non-existing property and no default value is provided
 		*/
 		UNKNOWNENGINE_SIMPLE_EXCEPTION ( PropertyNotFoundException );
-		
+
 		/**
 		 * @brief The properties map able to hold any value.
 		 *
@@ -49,8 +49,97 @@ namespace UnknownEngine
 		private:
 			typedef PropertiesTree<K> self_type;
 			typedef boost::variant<int, float, std::string, bool, boost::recursive_wrapper<self_type>, boost::any> VariantType;
+			typedef std::unordered_map<K, VariantType> MapType;
 			
 		public:
+			
+			/**
+			 * @brief Iterator class to iterate over all properties of the specified type
+			 */
+			template<typename V>
+			class Iterator
+			{
+			public:
+				
+				Iterator():
+				internal_pointer(nullptr)
+				{}
+				
+				explicit Iterator(PropertiesTree<K>* tree_to_iterate):
+				tree_to_iterate(tree_to_iterate),
+				internal_iter(tree_to_iterate->values.begin()),
+				internal_pointer(nullptr)
+				{
+					findNext();
+				};
+				
+				Iterator(const Iterator<V> &iter):
+				tree_to_iterate(iter.tree_to_iterate), 
+				internal_iter(iter.internal_iter),
+				internal_pointer(nullptr)
+				{};
+				
+				std::pair<K, V>& operator*()
+				{
+					return *internal_pointer;
+				}
+				
+				const std::pair<K, V>& operator*() const
+				{
+					return *internal_pointer;
+				}
+
+				Iterator<V>& operator++()
+				{
+					findNext();
+					return *this;
+				}
+				
+				Iterator<V>& operator++(int)
+				{
+					Iterator<V> result(*this);
+					findNext();
+					return result;
+				}
+				
+				bool operator==(const Iterator<V>& rhs)
+				{
+					return internal_iter == rhs.internal_iter;
+				}
+				
+				Iterator<V>& operator=(const Iterator<V>& rhs)
+				{
+					this->internal_iter = rhs.intenal_iter;
+					this->tree_to_iterate = rhs.map_to_iterate;
+				}
+				
+			private:
+
+				
+				void findNext()
+				{
+					bool found = false;
+					while(!found && internal_iter != tree_to_iterate->end())
+					{
+						++internal_iter;
+						V* pointer = boost::get<V>(&internal_iter->second);
+						if(pointer == nullptr)
+						{
+							const boost::any* any_val_pointer = boost::get<boost::any>(&internal_iter->second);
+							pointer = boost::any_cast<V>(any_val_pointer);
+						}
+						found = (internal_pointer != nullptr);
+						if(found) internal_pointer = &(*internal_iter);
+					}
+				}
+				
+				std::pair<K, V>* internal_pointer;
+				PropertiesTree<K>* tree_to_iterate;
+				
+				typedef typename PropertiesTree<K>::MapType::iterator InternalIteratorType;
+				
+				InternalIteratorType internal_iter;
+			};
 			
 			/**
 			* @brief Returns a value of specified type or throws exception
@@ -150,7 +239,7 @@ namespace UnknownEngine
 			UNKNOWNENGINE_INLINE
 			self_type* get_child_pointer(const K& name)
 			{
-				return get_pointer<self_type>(name);
+				return const_cast<self_type*>(get_pointer<self_type>(name));
 			}
 
 			/**
@@ -165,8 +254,27 @@ namespace UnknownEngine
 				values[name] = value;
 			}
 			
+			/**
+			 * @brief Returns the iterator pointing to the first value of specified type
+			 */
+			template<typename V>
+			Iterator<V> begin()
+			{
+				return Iterator<V>(&values);
+			}
+			
+			/**
+			 * @brief Returns the iterator pointing to the end of properties map
+			 */
+			template<typename V>
+			Iterator<V> end()
+			{
+				return Iterator<V>(&values, values.end());
+			}
+			
 		private:
-			std::unordered_map<K, VariantType> values;
+			template<typename V> friend class Iterator;
+			MapType values;
 		};
 
 	} /* namespace Core */
