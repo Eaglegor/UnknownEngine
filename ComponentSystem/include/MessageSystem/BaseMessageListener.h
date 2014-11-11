c32f99f6136f668e027580ceeaee41106611d80e
Eaglegor <eaglegor@gmail.com>
2014-11-11 20:48:12 +0400

    Simplified message buffer creation method

--- ComponentSystem/include/MessageSystem/BaseMessageListener.h
+++ ComponentSystem/include/MessageSystem/BaseMessageListener.h
@@ -42,6 +42,39 @@ namespace UnknownEngine
 			COMPONENTSYSTEM_EXPORT
 			void registerSupportedMessageType( const MessageType& message_type_id, IMessageReceivePolicy* receive_policy);
 
+			template<typename MessageClass, typename BufferClass, typename... Args>
+			bool createMessageBuffer(const Args&&... buffer_constructor_parameters)
+			{
+				static_assert(std::is_base_of<Utils::IMessageBuffer, BufferClass>::value, "Message buffer must implement Utils::IMessageBuffer");
+				static_assert(std::is_base_of<Core::Message, MessageClass>::value, "Message class must inherit from Core::Message");
+				
+				auto iter = received_messages.find(MESSAGE_TYPE_ID(MessageClass::getTypeName()));
+				if (iter == received_messages.end()) {
+					LOG_DEBUG(log_helper, "Message slot not found at listener: " + getName() + " Message type: " + std::string(MessageClass::getTypeName()) +
+						". Listener is not listening to this message type.");
+					return false;
+				}
+
+				LOG_DEBUG(log_helper, "Found message slot - creating buffer");
+
+				iter->second.message_buffer.reset(new BufferClass(std::forward<Args...>(buffer_constructor_parameters)));
+
+				LOG_DEBUG(log_helper, "Message buffer created");
+
+				return true;
+			}
+
+			template<typename CallbackOwnerClass, typename MessageClass, typename BufferClass, typename... Args>
+			bool createMessageBuffer(CallbackOwnerClass* callback_owner_class, void (CallbackOwnerClass::*callback_method)(const MessageClass&))
+			{
+
+				typedef std::function<void(const MessageClass&)> MessageProcessor;
+				MessageProcessor callback = std::bind(callback_method, callback_owner_class, std::placeholders::_1);
+
+				return createMessageBuffer<MessageClass, BufferClass>(callback);
+			}
+
+			
 			template<typename BufferClass>
 			bool registerMessageBuffer( const BufferClass &buffer)
 			{
@@ -54,12 +87,13 @@ namespace UnknownEngine
 
 				LOG_DEBUG(log_helper, "Message buffer found, ...");
 
-				iter->second.setMessageBuffer( buffer );
+				iter->second.message_buffer.reset(new BufferClass( buffer ) );
 
 				LOG_DEBUG(log_helper, "Buffer registered...");
 
 				return true;
 			}
+			
 
 			COMPONENTSYSTEM_EXPORT
 			MessagingPoliciesManager& getMessagingPoliciesManager();
@@ -91,12 +125,6 @@ namespace UnknownEngine
 				
 				ReceivedMessage(const ReceivedMessage &msg):
 				receive_policy(msg.receive_policy){};
-
-				template<typename T>
-				void setMessageBuffer(const T& new_message_buffer)
-				{
-					message_buffer.reset(new T(new_message_buffer));
-				}
 
 			};
 
