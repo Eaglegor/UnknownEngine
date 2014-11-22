2b69d2cb79e0e64406537b8ab6b6d63eb8e5d9e6
Eaglegor <eaglegor@gmail.com>
2014-11-22 13:20:41 +0400

    Added elapsed time to the time counter

--- Utils/include/Profiling/TimeCounter.h
+++ Utils/include/Profiling/TimeCounter.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Scalar.h>
 #include <chrono>
 
 namespace UnknownEngine
@@ -9,22 +10,38 @@ namespace UnknownEngine
 		class TimeCounter
 		{
 		public:
-			float getDt()
+			TimeCounter():
+			dt(0),
+			elapsed(0)
+			{}
+			
+			Math::Scalar getDt()
 			{
 				return dt;
+			}
+			
+			void resetElapsedTime()
+			{
+				elapsed = 0;
+			}
+			
+			Math::Scalar getElapsedTime()
+			{
+				return elapsed;
 			}
 			
 			void tick()
 			{
 				std::chrono::steady_clock::time_point newTime = std::chrono::steady_clock::now();
-				dur += newTime - old;
+				dur = newTime - old;
 				old = newTime;
 				dt = dur.count();
-				dur = dur.zero();
+				elapsed += dt;
 			}
 			
 		private:
-			float dt;
+			Math::Scalar dt;
+			Math::Scalar elapsed;
 			std::chrono::duration<float> dur;
 			std::chrono::steady_clock::time_point old;
 		};
